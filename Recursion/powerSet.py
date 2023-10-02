ddac664dcbcca1b4eb88b1a4379db805edd92cef
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2023-10-02 12:40:43 +0530

    Create powerSet.py

---
+++ Recursion/powerSet.py
@@ -0,0 +1,17 @@
+# problem statement :https://www.algoexpert.io/questions/powerset
+def powersetHelper(array,idx,curr_powerset,ans):
+    if idx == len(array):
+        ans.append(curr_powerset)
+        return
+    #include
+    powersetHelper(array,idx+1,curr_powerset+[array[idx]],ans)
+    #exclude
+    powersetHelper(array,idx+1,curr_powerset,ans)
+  
+def powerset(array):
+    # Write your code here.
+    ans =[]
+    curr_powerset =[]
+    idx =0
+    powersetHelper(array,idx,curr_powerset,ans)
+    return ans
